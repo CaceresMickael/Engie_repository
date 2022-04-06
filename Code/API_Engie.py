d4bd94754ab22a4ab3b0b822c74f8f77d57c7b31
Mickaël Caceres <caceres.mickael1@gmail.com>
2022-04-06 18:42:23 +0200

    requirements update

--- Code/API_Engie.py
+++ Code/API_Engie.py
@@ -1,11 +1,10 @@
+# LIBRARY IMPORTATION
 from flask import Flask, render_template, request, jsonify
 import json
 import pandas as pd
 
 app = Flask(__name__)
-# app.config[r'C:\Users\MICACERE\Documents\Challenge_Engie']
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-# Function 
 
 @app.route('/')
 def Welcoming_text():
@@ -21,6 +20,7 @@ def uploader_file():
       f = request.files['file']
       payload = json.load(f)
 
+      # Function to calculate the cost per MWh
       def function_cost_1MWh(x):
          if x['type'] == "gasfired":
             return round((gas_euro_MWh * (1 / x["efficiency"])) +  (0.3 * co2_euro_ton),2)
@@ -28,25 +28,34 @@ def uploader_file():
             return round(kerosine_euro_MWh * (1 / x["efficiency"]),2) # Do we need to add the C02 ?
          elif x['type'] == 'windturbine':
             return 0
-      
+
+      # Convert the Json File to a panda tab
       df_powerplants = pd.DataFrame.from_dict(payload["powerplants"])
+
+      # Extraction of important value
       gas_euro_MWh = payload["fuels"]["gas(euro/MWh)"]
       kerosine_euro_MWh = payload["fuels"]["kerosine(euro/MWh)"]
       co2_euro_ton = payload["fuels"]["co2(euro/ton)"]
       wind_percentage = payload["fuels"]["wind(%)"]
       load_max  = payload["load"]
+
+      # Calculation of 1MWh and pmax for the windturbine(considering wind%)
       df_powerplants["cost_1MWh"] = df_powerplants.apply(function_cost_1MWh, axis =1)
 
       for index, row in df_powerplants.iterrows():
          if row["type"] == "windturbine":
             df_powerplants.at[index , "pmax"] = round(row["pmax"] * (wind_percentage/100),1)
 
+      # Creation of two new columns to fill in later
       df_powerplants["load"] = load_max
       df_powerplants["p"] = 0
 
+      # Reset the Index reset to have the less expensive methods in the first row
       df_powerplants = df_powerplants.sort_values('cost_1MWh').reset_index() # Should I add pmin ?
       df_powerplants.drop('index', inplace=True, axis=1)
 
+      #The main idea here is to work only on all the row where load is superior to the cumul of pmax plus one row.
+      #On this rows, the pmin is set on the p.
       load_remain = load_max
       df_powerplants["pmax_cumul"] = df_powerplants["pmax"].cumsum()
       for index, row in df_powerplants.iterrows():
@@ -54,6 +63,7 @@ def uploader_file():
             df_powerplants.at[index , "p"] = row["pmin"]
             load_remain -= row["pmax"]
 
+      # Like the p have been set on pmin on some row, we create a new variable updated without the p.
       load_calculate = load_max - df_powerplants["p"].sum() 
       for index, row in df_powerplants.iterrows():
          #if pmax is superior or equal to the load
@@ -74,11 +84,10 @@ def uploader_file():
             load_calculate -=  load_calculate
             #print("c",index, load_calculate, row["p"], row["pmax"]) # debug checker
 
+      # Calculate the total cost of each powerplants
       df_powerplants["total_cost"] = df_powerplants.apply(lambda x: round(x["p"] * x["cost_1MWh"],2), axis = 1)
 
-      #df_list = df_powerplants.values.tolist() jsonpify(df_list)
-
-      #result = df_powerplants[["name", "p", "total_cost"]].to_json(orient="table", index= False)
+      # Create a dictionnary and return it in json
       result = df_powerplants[["name", "p", "total_cost"]].to_dict('index')
       return jsonify(result)
 
